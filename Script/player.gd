8281e825292fa7c9c995b6d4012e4a9dcaba5ed2
itsFahmid <fahmidsiddiqueahmed@gmail.com>
2025-08-13 16:14:35 +0600

    added enemy moves, improved player death

--- Script/player.gd
+++ Script/player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 
 func _physics_process(delta: float) -> void:
